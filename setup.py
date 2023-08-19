d502408da89af34fe5db12697cceb842ad087bb4
Suraj Parmar <parmarsuraj99@gmail.com>
2023-08-18 22:41:29 -0400

    add requests to requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+print(setuptools.find_packages())
+
 setuptools.setup(
     name="signalslite",
     version="0.0.1",
@@ -18,5 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
